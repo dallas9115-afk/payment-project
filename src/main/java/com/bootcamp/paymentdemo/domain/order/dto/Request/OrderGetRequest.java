07077aa7f40e4111d96efc5214ccdfa362aacbf9
godchm <choehyeongmin7@gmail.com>
2026-03-17 18:28:05 +0900

    임시 저장

--- src/main/java/com/bootcamp/paymentdemo/domain/order/dto/Request/OrderGetRequest.java
+++ src/main/java/com/bootcamp/paymentdemo/domain/order/dto/Request/OrderGetRequest.java
@@ -3,7 +3,9 @@ package com.bootcamp.paymentdemo.domain.order.dto.Request;
 import lombok.Getter;
 
 @Getter
-public class OrderRequest {
+public class OrderGetRequest {
+
+//    private String orderNumber;
 
 
 
