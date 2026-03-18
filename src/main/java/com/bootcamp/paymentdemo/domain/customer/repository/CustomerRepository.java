b2f8ba98ec6acb52f6539a7aa3bb2028d8ea71a4
hyun2ee2 <Hongkobi04@gmail.com>
2026-03-18 16:27:18 +0900

    feat: 존재하는 이메일인지 검증하는 로직 추가

--- src/main/java/com/bootcamp/paymentdemo/domain/customer/repository/CustomerRepository.java
+++ src/main/java/com/bootcamp/paymentdemo/domain/customer/repository/CustomerRepository.java
@@ -4,7 +4,11 @@ import com.bootcamp.paymentdemo.domain.customer.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
     boolean existsByEmail(String email);
+
+    Optional<Customer> findByEmail(String email);
 }
