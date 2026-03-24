a2f95d122d2ed718ccd24f14a2a061336c8f6749
cherry pick <limhojin956@naver.com>
2026-03-24 22:03:55 +0900

    hotfix

--- src/main/java/com/bootcamp/paymentdemo/domain/subscription2/repository/SubscriptionBillingRepository2.java
+++ src/main/java/com/bootcamp/paymentdemo/domain/subscription2/repository/SubscriptionBillingRepository2.java
@@ -20,8 +20,6 @@ public interface SubscriptionBillingRepository2 extends JpaRepository<Subscripti
 
     boolean existsBySubscriptionAndScheduledDate(Subscription2 subscription, LocalDateTime scheduledDate);
 
-    boolean existsByCustomerIdAndPlanIdAndScheduledDate(Long customerId, Long planId, LocalDateTime today);
-
     @Query("SELECT COUNT(b) > 0 FROM SubscriptionBilling2 b " +
             "WHERE b.subscription.customerId = :customerId " +
             "AND b.subscription.plan.id = :planId " +
