2dc042782c52b02fb613641cb055c8baaefe8357
cherry pick <limhojin956@naver.com>
2026-03-26 22:52:01 +0900

    데드코드 삭제

--- src/main/java/com/bootcamp/paymentdemo/domain/point/repository/Repository.java
+++
@@ -1,4 +0,0 @@
-package com.bootcamp.paymentdemo.domain.point.repository;
-
-public interface Repository {
-}
--- src/main/java/com/bootcamp/paymentdemo/domain/point/service/Service.java
+++
@@ -1,4 +0,0 @@
-package com.bootcamp.paymentdemo.domain.point.service;
-
-public class Service {
-}
